c8132052219977e2e9a2c562b0eebb265da5c5e3
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 14:52:23 +0100

    Build the for loop and comment out code attempting for user input
    Completely stop committing changes because this is the first time trying
    to work something out myself while also using git - a lot of practice
    needed in this area

--- NumberSeriesSum/src/ie/atu/sw/NumberSeriesSum.java
+++ NumberSeriesSum/src/ie/atu/sw/NumberSeriesSum.java
@@ -1,25 +1,54 @@
 package ie.atu.sw;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class NumberSeriesSum {
 	
 	//INSTANCE VARIABLES
-	private Scanner scanner;
-	private int max_number;
-	private int min_number;
+	//private Scanner scanner;
+	private int min_number = 20; //minInput();
+	private int max_number = 60;//maxInput();
+	int sum = 0;
 
 	//CONSTRUCTOR
 	public NumberSeriesSum() {
 		
-		scanner = new Scanner(System.in);
+		//scanner = new Scanner(System.in);
+		
+		start();
 		
 	}
+	/*
+	public int minInput() {
+		
+		System.out.println("Enter the MIN number in the series sum>");
+		String min_input = scanner.next();
+		int min = (int)Double.parseDouble(min_input);
+		
+		return min;
+	}
 	
+	public int maxInput() {
+		
+		System.out.println("Enter the MAX number in the series sum>");
+		String max_input = scanner.next();
+		int max = (int)Double.parseDouble(max_input);
+		
+		return max;
+	}
+	*/
 	public void start() {
 		System.out.println("--------------------------------");
 		System.out.println("-- Sum of a Series of Numbers --");
 		System.out.println("--------------------------------");
+		
+		//minInput();
+		//maxInput();
+		
+		for (int i = min_number; i <= max_number; i++) {
+			sum += i;
+		}
+		
+		System.out.println("Sum: " + sum);
 	}
-
 }
