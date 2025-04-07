eafe17a8b810dae9438e7bfe1b9fa5f13e83083d
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 13:05:20 +0100

    Add instance variables for capturing user input of min and max values

--- NumberSeriesSum/src/ie/atu/sw/NumberSeriesSum.java
+++ NumberSeriesSum/src/ie/atu/sw/NumberSeriesSum.java
@@ -1,6 +1,13 @@
 package ie.atu.sw;
 
+import java.util.Scanner;
+
 public class NumberSeriesSum {
+	
+	//INSTANCE VARIABLES
+	private Scanner scanner;
+	private int max_number;
+	private int min_number;
 
 	//CONSTRUCTOR
 	public NumberSeriesSum() {
